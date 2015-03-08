69d38772e9ade5808c688a683bb7a2e0a2bd768b
Matteo Menapace <hello@baddeo.com>
2015-03-08 18:00:08 +0000

    Students data on its own file

---
+++ sessions/html5-boilerplate-random-student/data/students.js
@@ -0,0 +1,71 @@
+var students = 
+[
+	{
+		name: 'Kieran Bailey',
+		imageId: '106430'
+	},
+	{
+		name: 'Ryder Barnes',
+		imageId: '106445'
+	},
+	{
+		name: 'Harry Difolco',
+		imageId: '106450'
+	},
+	{
+		name: 'Joe Dunkerley',
+		imageId: '106454'
+	},
+	{
+		name: 'Alexander Farmery',
+		imageId: '106448'
+	},
+	{
+		name: 'Jonny Gwillim',
+		imageId: '106383'
+	},
+	{
+		name: 'Luke Henry',
+		imageId: '106534'
+	},
+	{
+		name: 'Isaac Houghton',
+		imageId: '106451'
+	},
+	{
+		name: 'Juliette Jupp',
+		imageId: '106434'
+	},
+	{
+		name: 'Rana Karim',
+		imageId: '106435'
+	},
+	{
+		name: 'Jack Kemmish',
+		imageId: '106427'
+	},
+	{
+		name: 'Aidan Mcvay',
+		imageId: '106436'
+	},
+	{
+		name: 'Ollie Norris',
+		imageId: '106437'
+	},
+	{
+		name: 'Sanj Pardesi',
+		imageId: '106439'
+	},
+	{
+		name: 'Tom Sharman',
+		imageId: '106443'
+	},
+	{
+		name: 'Johan Wilhelmsson',
+		imageId: '106447'
+	},
+	{
+		name: 'Harry Wills',
+		imageId: '106449'
+	}
+]
--- sessions/html5-boilerplate-random-student/js/main.js
+++ sessions/html5-boilerplate-random-student/js/main.js
@@ -39,7 +39,7 @@ function studentTemplate(student)
 	return html
 }
 
-function listStudents()
+function listStudents(students)
 {
 	var randomStudents = $.extend(true, [], students).shuffle(), // clone the students array and then shuffle it
 		$ul = $('ul#students')
@@ -50,75 +50,3 @@ function listStudents()
 		$ul.append(li)
 	})
 }
-
-var students = 
-[
-	{
-		name: 'Kieran Bailey',
-		imageId: '106430'
-	},
-	{
-		name: 'Ryder Barnes',
-		imageId: '106445'
-	},
-	{
-		name: 'Harry Difolco',
-		imageId: '106450'
-	},
-	{
-		name: 'Joe Dunkerley',
-		imageId: '106454'
-	},
-	{
-		name: 'Alexander Farmery',
-		imageId: '106448'
-	},
-	{
-		name: 'Jonny Gwillim',
-		imageId: '106383'
-	},
-	{
-		name: 'Luke Henry',
-		imageId: '106534'
-	},
-	{
-		name: 'Isaac Houghton',
-		imageId: '106451'
-	},
-	{
-		name: 'Juliette Jupp',
-		imageId: '106434'
-	},
-	{
-		name: 'Rana Karim',
-		imageId: '106435'
-	},
-	{
-		name: 'Jack Kemmish',
-		imageId: '106427'
-	},
-	{
-		name: 'Aidan Mcvay',
-		imageId: '106436'
-	},
-	{
-		name: 'Ollie Norris',
-		imageId: '106437'
-	},
-	{
-		name: 'Sanj Pardesi',
-		imageId: '106439'
-	},
-	{
-		name: 'Tom Sharman',
-		imageId: '106443'
-	},
-	{
-		name: 'Johan Wilhelmsson',
-		imageId: '106447'
-	},
-	{
-		name: 'Harry Wills',
-		imageId: '106449'
-	}
-]
