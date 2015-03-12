1dfe7ed19c1efcb2f0fe5de720bd9a158f34df7a
Matteo Menapace <hello@baddeo.com>
2015-03-12 12:57:04 +0000

    Extended native JS Array with functions to shuffle elements and return a random element

--- sessions/html5-boilerplate-student-shuffler/js/plugins.js
+++ sessions/html5-boilerplate-student-shuffler/js/plugins.js
@@ -22,3 +22,33 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+// from http://stackoverflow.com/a/24137301/4647178
+Array.prototype.getRandomElement = function () 
+{
+    return this[Math.floor((Math.random()*this.length))]
+}
+
+// from http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 
+Array.prototype.shuffle = function () 
+{
+    var currentIndex = this.length, 
+        temporaryValue, 
+        randomIndex
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) 
+    {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex -= 1
+
+        // And swap it with the current element.
+        temporaryValue = this[currentIndex]
+        this[currentIndex] = this[randomIndex]
+        this[randomIndex] = temporaryValue
+    }
+
+    return this
+}
